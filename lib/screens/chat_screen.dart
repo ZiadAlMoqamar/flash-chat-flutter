1bdb6e5167d496711ab67e7d410de4741f21cefb
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2020-12-14 20:55:25 +0200

    adding stream for messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -33,6 +33,20 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').get();
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+
+void messagesStream() async{
+  await for ( var snapshot in _firestore.collection('messages').snapshots()){
+    for (var message in snapshot.docs){
+      print(message.data());
+    }
+  }
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,8 +84,8 @@ class _ChatScreenState extends State<ChatScreen> {
                   FlatButton(
                     onPressed: () {
                       _firestore.collection('messages').add({
-                      'text': messageText,
-                      'sender': loggedInUser.email,
+                        'text': messageText,
+                        'sender': loggedInUser.email,
                       });
                     },
                     child: Text(
