95a4eaf4348efe53b32aed57344238ac46bd01d0
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2020-12-15 00:15:13 +0200

    adding circular loading indicator to stream

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -33,7 +33,6 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,9 +55,15 @@ class _ChatScreenState extends State<ChatScreen> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('messages').snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
+                stream: _firestore.collection('messages').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        backgroundColor: Colors.lightBlueAccent,
+                      ),
+                    );
+                  }
                   final messages = snapshot.data.docs;
                   List<Text> messageWidgets = [];
                   for (var message in messages) {
@@ -72,9 +77,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   return Column(
                     children: messageWidgets,
                   );
-                }
-              },
-            ),
+                }),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
